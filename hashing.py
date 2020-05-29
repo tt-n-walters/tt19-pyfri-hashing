a3cc04a97e57a655d1573d6b9faa83574b5a9157
nw <nico.walters@techtalents.es>
2020-05-29 17:57:24 +0200

    Hashing steps

--- hashing.py
+++ hashing.py
@@ -4,10 +4,12 @@ import hashlib
 name = "Nico"
 
 # Encode our input
-
+encoded = name.encode("utf-8")
 
 # Hash our encoding
-
+hashed = hashlib.md5(encoded)
 
 # Get the digest of the hash
+digest = hashed.hexdigest()
 
+print(digest)
