669c15f14e3f6057739a3557a5fd6a59bfe02d12
nw <nico.walters@techtalents.es>
2020-05-29 18:14:09 +0200

    Hashing performance check

--- hashing.py
+++ hashing.py
@@ -1,18 +1,25 @@
 import hashlib
+import timeit
 
 # Deterministic
 # Chaotic
 # Non-reversible
 
+start_time = timeit.default_timer()
+end_time = start_time + 5
+
 name = "Mico"
+counter = 0
 
-# Encode our input
-encoded = name.encode("utf-8")
+while timeit.default_timer() < end_time:
+    # Encode our input
+    encoded = name.encode("utf-8")
+    # Hash our encoding
+    hashed = hashlib.md5(encoded)
+    # Get the digest of the hash
+    digest = hashed.hexdigest()
+    counter += 1
 
-# Hash our encoding
-hashed = hashlib.md5(encoded)
 
-# Get the digest of the hash
-digest = hashed.hexdigest()
 
 print(digest)
