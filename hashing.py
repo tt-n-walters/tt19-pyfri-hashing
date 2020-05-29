c8b35b38bded58f1dee9365d1e5d5d33e44b9997
nw <nico.walters@techtalents.es>
2020-05-29 18:29:37 +0200

    Preparing for multiprocessing

--- hashing.py
+++ hashing.py
@@ -5,23 +5,24 @@ import timeit
 # Chaotic
 # Non-reversible
 
-start_time = timeit.default_timer()
-end_time = start_time + 10
+def calculate_hashes():
+    start_time = timeit.default_timer()
+    end_time = start_time + 20
 
-name = "Mico"
-counter = 0
+    name = "Mico"
+    counter = 0
 
-print("Starting hashes.")
-while timeit.default_timer() < end_time:
-    # Encode our input
-    encoded = name.encode("utf-8")
-    # Hash our encoding
-    hashed = hashlib.md5(encoded)
-    # Get the digest of the hash
-    digest = hashed.hexdigest()
-    counter += 1
+    print("Starting hashes.")
+    while timeit.default_timer() < end_time:
+        # Encode our input
+        encoded = name.encode("utf-8")
+        # Hash our encoding
+        hashed = hashlib.md5(encoded)
+        # Get the digest of the hash
+        digest = hashed.hexdigest()
+        counter += 1
 
-print("Finished hashes.")
-print("Hashed {:,} times in 10 seconds.".format(counter))
-print("Average of {:,} hashes per second.".format(counter / 10))
+    print("Finished hashes.")
+    print("Hashed {:,} times in 20 seconds.".format(counter))
+    print("Average of {:,} hashes per second.".format(counter / 20))
 
